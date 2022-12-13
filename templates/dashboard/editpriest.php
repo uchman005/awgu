5623c20e7c3ebf743f7473f459fe7009a780ca61
Chinwenma <okoriechinwe1327@gmail.com>
2022-12-13 12:51:17 -0800

    created edits

---
+++ templates/dashboard/editpriest.php
@@ -0,0 +1,68 @@
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Edit Priest</h1>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <!-- left column -->
+                <div class="col-md-12">
+                    <!-- general form elements -->
+                    <div class="card card-success">
+                        <div class="card-header">
+                            <h3 class="card-title"> Priest</h3>
+                        </div>
+                        <!-- /.card-header -->
+                        <!-- form start -->
+                        <form action="./users/create" method="post" enctype="multipart/form-data">
+                            <div class="card-body">
+                                <div class="form-group">
+                                    <label for="">Full Name</label>
+                                    <input type="text" class="form-control" placeholder="Enter fullname" name="name">
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1">Email address</label>
+                                    <input type="email" name="email" class="form-control" id="email" placeholder="Enter email">
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputPassword1">Password</label>
+                                    <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                                </div>
+                                <div class="form-group">
+                                    <label for="date">Date of Ordination</label>
+                                    <input type="date" class="form-control" placeholder="Enter date" name="dod">
+                                </div>
+                                <div class="form-group">
+                                    <label for="parish">Parish</label>
+                                    <input type="text" class="form-control" placeholder="Enter parish" name="parish">
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputFile">Passport</label>
+                                    <div class="input-group">
+                                        <div class="custom-file">
+                                            <input type="file" name="img" class="custom-file-input" id="exampleInputFile">
+                                            <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                                        </div>
+                                    
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-footer">
+                                <button type="submit" class="btn btn-success btn-block">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
---
+++ templates/dashboard/editreflection.php
@@ -0,0 +1,77 @@
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Edit Reflection</h1>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <!-- left column -->
+                <div class="col-md-12">
+                    <!-- general form elements -->
+                    <div class="card card-success">
+                        <div class="card-header">
+                            <h3 class="card-title">Edit Reflection</h3>
+                        </div>
+                        <!-- /.card-header -->
+                        <!-- form start -->
+                        <form>
+                            <div class="card-body">
+                                <div class="form-group">
+                                    <label for="reflectiontitle">Title</label>
+                                    <input type="text" class="form-control" placeholder="Enter title" name="name">
+                                </div>
+                                <div class="form-group">
+                                    <label for="flectionauthor">Author</label>
+                                    <input type="email" class="form-control" placeholder="Writer`s  name">
+                                </div>
+                                <div class="form-group">
+                                    <label for="body">Body</label>
+                                    <input type="text" class="form-control"  placeholder="write your reflection">
+                                </div>
+                                <div class="form-group">
+                                    <label for="image1">Image1</label>
+                                    <div class="input-group">
+                                        <div class="custom-file">
+                                            <input type="file" class="custom-file-input">
+                                            <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                                        </div>
+                                        <div class="input-group-append">
+                                            <span class="input-group-text">Upload</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="image2">Image2</label>
+                                    <div class="input-group">
+                                        <div class="custom-file">
+                                            <input type="file" class="custom-file-input">
+                                            <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                                        </div>
+                                        <div class="input-group-append">
+                                            <span class="input-group-text">Upload</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="image1">Date</label>
+                                    <input type="date" class="form-control" placeholder="Enter date" name="dod">
+                                </div>
+                            <div class="card-footer">
+                                <button type="submit" class="btn btn-success btn-block">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
